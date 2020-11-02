941a96df1ba7e55840b382ea35dd2828b9651bee
unknown <wilhamsmeira@gmail.com>
2020-11-01 22:37:59 -0300

    update files

--- aulas/backend/src/models/Spot.js
+++ aulas/backend/src/models/Spot.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 /**Lembrete: instalamos uma ferramenta chamada multer com yarn add multer, ela serve pra lidar com formatos Multipart form, que serve pra lidar com uploads de imagens
- * dentro da pasta config temos um araquivo chamado upload com as configurações dessa lib
+ * dentro da pasta config temos um arquivo chamado upload com as configurações dessa lib
   */
 
 const SpotSchema = new mongoose.Schema({
