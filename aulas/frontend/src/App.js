29d8136f5dad30f3b1aed65cfa1c5f5a89e7fc82
unknown <wilhamsmeira@gmail.com>
2020-11-01 22:32:19 -0300

    add imagem

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -3,13 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <p>
-          Wilhams
-        </p>
-      </header>
-    </div>
+    <div className="container"></div>
   );
 }
 
