e43cbdae1211c678e2132474646c4471ab885c1f
unknown <wilhamsmeira@gmail.com>
2020-11-01 23:19:42 -0300

    css e html da página principal

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="container"></div>
+    <div className="container">
+      <img src={logo} alt="Logo da AirCnC"/> {/**para incluir um código JS dentro do html, temos q usar chaves */}
+
+      <div className="content">
+      <p>
+      Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+      </p>
+      
+      <form>
+        <label htmlFor="email">E-MAIL *</label>
+        <input 
+          id="email" 
+          type="email" 
+          placeholder="Seu melhor e-mail"
+    
+        />
+
+        <button className="btn" type="submit">Entrar</button>
+      </form>
+     </div>
+    </div>
   );
 }
 
