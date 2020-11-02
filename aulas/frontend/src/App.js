0405f00a3857bf1b7e7a477a7e55dbdcd6b36fc3
unknown <wilhamsmeira@gmail.com>
2020-11-02 13:12:10 -0300

    save localStorage

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -15,6 +15,15 @@ function App() {
     const response = await api.post('/sessions', { email: email }); //em casos que a chave e o valor são os mesmos, podemos colocar apenas campo com email ao invés de dois, deixei os dois pra ser mostrado como se faz a maneira tradicional
     console.log(response);   //nesse console irá mostrar um erro e não será possivel enviar a requisição pra api, o CORS bloqueia isso, pq o CORS protege sua API pra que niguém consuma sua API e roube seus dados
                             //comando =>  npm install cors ou yarn add cors se o yarn tiver instalado
+
+    //Caso queira só pegar o _id de dentro do response com destructuring
+    const { _id } = response.data;
+    console.log(_id); 
+    
+    //localStorage => funciona como um banco de dados dentro do nosso browser
+    //apenas com essa linha abaixo, já estará salvo o id na nossa aplicação
+    //vá na aba Application do devtools e procure localstorage e verá a informação lá
+    localStorage.setItem('user', _id); 
   }
 
   return (
