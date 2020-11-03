edefed577ca9416cf90848446bdca7c123df7ecd
Wilhams Junior <wilhamsmeira@gmail.com>
2020-11-03 18:20:04 -0300

    comments

--- aulas/backend/src/controllers/DashboardController.js
+++ aulas/backend/src/controllers/DashboardController.js
@@ -8,6 +8,7 @@ module.exports = {
     async show(req, res) { 
         const { user_id } = req.headers; //req.headers => é o id guardado no localStorage no momento q crio o usuário
 
+        //Spot.find => procure no Spot.js algum usuário com esse id => user: user_id, e me traga tudo sobre ele
         const spots = await Spot.find({ user: user_id });  //Spot => é o requerido acima na linha 1
                                                           //user_id => é o type: mongoose.Schema.Types.ObjectId dentro Spot.js, o mongoose gera essa id de forma automática
                                                          //user => é a chave dentro de Spot.js  
