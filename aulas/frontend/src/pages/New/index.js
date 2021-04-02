04cafedcad892895a2b15fc7b977089cc3db31ab
Wilhams Junior <wilhamsmeira@gmail.com>
2021-04-02 17:31:09 -0300

    feito o formulario

--- aulas/frontend/src/pages/New/index.js
+++ aulas/frontend/src/pages/New/index.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function New() {
-    return <div></div>
+
+    const [company, setCopany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+    const enviar = () => {
+
+    }
+
+    return (
+        <form onSubmit={enviar}>
+            <label htmlFor="company" className="">EMPRESA *</label>
+            <input
+                id="company"
+                placeholder="Sua empresa"
+                value={company}
+                onChange={e => setCopany(e.target.value)}
+            />
+
+                <label htmlFor="company" className="">TECNOLOGIAS * <span>(separadas por vírgulas)</span></label>
+                <input
+                    id="techs"
+                    placeholder="Quais tecnologias vcs usam? "
+                    value={techs}
+                    onChange={e => setTechs(e.target.value)}
+                />
+
+                    <label htmlFor="company" className="">VALOR DA DIÁRIA * <span>(EM BRANCO PARA GRATUITO)</span></label>
+                    <input
+                        id="price"
+                        placeholder="Valor cobrado por dia "
+                        value={price}
+                        onChange={e => setPrice(e.target.value)}
+                    />
+
+                <button type="submit" className="btn">Cadastrar</button>    
+                </form>
+    )
 }
